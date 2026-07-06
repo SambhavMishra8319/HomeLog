c8453dac1ee6fe16c0f37450a34b4deac4f80b10
Sambhav Mishra <sammishr8319@gmail.com>
2026-07-07 00:48:25 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,15 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBhe8Il7vlG56Stf3dDGugX_IzY93KTio8",
-  authDomain: "gharrent-manager.firebaseapp.com",
-  projectId: "gharrent-manager",
-  storageBucket: "gharrent-manager.firebasestorage.app",
-  messagingSenderId: "532092114308",
-  appId: "1:532092114308:web:98dc2fe4164a5ce23879d1",
-  measurementId: "G-BJ487P1V5K"
-};
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
